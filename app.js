4abfe5857c0c46714dc879e5f5129ac4870dd6b9
91Savage <ekdma1403@gmail.com>
2022-09-20 00:58:21 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 23;
+const myName = 'sehun';
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('Hello' + myName);
+//
+myName = 'savage';
+
+console.log('your name is ' + myName);
