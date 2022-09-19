c4afb337cd30e334f16f0d3a2d8cb563f27e886f
91Savage <ekdma1403@gmail.com>
2022-09-20 01:41:50 +0900

    1

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const player = {
     name: 'sehun',
-    points: 10,
-    fat: true,
+    sayHello: function (otherPersonsName) {
+        console.log('Como Estas ' + otherPersonsName + ' hola ');
+    },
 };
-console.log(player);
-player.fat = false; // const 로 선언했지만 오브젝트 안의 내용은 변경 가능
-player.lastName = 'potato'; // 배열 추가 가능
-console.log(player);
 
-//console.log(player['name']);
+console.log(player.name);
+player.sayHello('savage');
