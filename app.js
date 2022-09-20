1b1b380a81a4347264f6aa5b7ecf1c7d3acbfb37
91Savage <ekdma1403@gmail.com>
2022-09-21 00:11:20 +0900

    1

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ let person = { fname: 'JOHN', lname: 'Doe', age: 25 };
 for (const key in person) {
     console.log(person[key]);
 }
+
+let brands = ['Apple', 'Google', 'facebook', 'amazon', 'Samsung'];
+
+for (const brand of brands) {
+    console.log(brand);
+}
+
+let language = 'JavaScript';
+for (const x of language) {
+    console.log(x);
+}
