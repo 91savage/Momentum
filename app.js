15061064e24b27e85ab621ba4933c508cd158d54
91Savage <ekdma1403@gmail.com>
2022-09-25 16:50:49 +0900

    1

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-let words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// const loginForm = document.querySelector('#login-form');
+// const loginInput = document.querySelector('#login-form input');
 
-words.filter((element, index, thisArg) => {
-    console.log(element, index, thisArg);
-});
+// function onLoginSubmit(event) {
+//     event.preventDefault();
+//     // const username = loginInput.value;
+//     console.log(loginInput.value);
+// }
+// loginForm.addEventListener('submit', onLoginSubmit); //submit이 되었을 때만 onLoginSubmit 실행
 
-let result = words.filter(function (a) {
-    console.log(a);
-    return a.length > 6;
-});
+const title = document.querySelector('div.helo:first-child h1');
 
-console.log(result);
+console.dir(title);
+
+function handleTitleClick() {
+    title.style.color = 'blue';
+}
+function handleMouseEnter() {
+    title.innerText = 'mouse is here!';
+}
+function handleMouseLeave() {
+    title.style.color = 'mouse is gone!';
+}
+
+title.addEventListener('click', handleTitleClick);
+title.addEventListener('mouseenter', handleMouseEnter);
+title.addEventListener('mouseleave', handleThandleMouseLeave);
