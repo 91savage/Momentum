7a0b4f387904ab1fe4545f16cef0a8e395379adb
91Savage <ekdma1403@gmail.com>
2022-10-02 23:33:10 +0900

    recap

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
-
+const USERNAME_KEY = 'username';
 const link = document.querySelector('a');
 
 function onLoginSubmit(event) {
     event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막기
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem('username', username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', onLoginSubmit); //submit이 되었을 때만 onLoginSubmit 실행
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
